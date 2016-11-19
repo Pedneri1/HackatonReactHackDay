f5c5043c14680d2562e77f2bf948de015526a425
pedroneri1 <pedneri1@gmail.com>
2016-11-19 17:25:13 -0300

    Init forward function to listview

--- adopt/Feed.js
+++ adopt/Feed.js
@@ -1,46 +1,56 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  ListView,
-  TouchableHighlight
-} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, ListView, TouchableHighlight} from 'react-native';
 
 import Row from './Row';
 var animais = [
-    { "raca": "Doberman",
-      "especie":" Cachorro",
-      "endereco": "Avenida Fraga maia",
-      "nascimento": "30/05/1996",
-      "sexo": "Femea",
-      "fotos": "",
-      "decricao": "Victor"},
-      { "raca": "Doberman",
-        "especie":" Cachorro",
-        "endereco": "Avenida Fraga maia",
-        "nascimento": "30/05/1996",
-        "sexo": "Femea",
-        "fotos": "",
-        "decricao": "Victor"}
-  ]
+  {
+    "raca": "Doberman",
+    "especie": " Cachorro",
+    "endereco": "Avenida Fraga maia",
+    "nascimento": "30/05/1996",
+    "sexo": "Femea",
+    "fotos": "",
+    "decricao": "Victor"
+  }, {
+    "raca": "Doberman",
+    "especie": " Cachorro",
+    "endereco": "Avenida Fraga maia",
+    "nascimento": "30/05/1996",
+    "sexo": "Femea",
+    "fotos": "",
+    "decricao": "Victor"
+  }
+]
 export default class Feed extends Component {
   constructor() {
     super();
-    var dataSource = new ListView.DataSource({rowHasChanged:(r1,r2) => r1.guid != r2.guid});
+    var dataSource = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1.guid != r2.guid
+    });
     this.state = {
       dataSource: dataSource.cloneWithRows(animais)
     }
   }
 
-  render(){
+  render() {
     return (
-      <ListView dataSource={this.state.dataSource}
-            renderRow={(dataSource)=> <Row
-                data = {dataSource}
-                />
-            }>
-      </ListView>
+      <Navigator
+        initialRoute={{ mark: 0 }}
+        renderScene={(route, navigator) => {
+        switch (route.mark) {
+          case 0:
+            return (
+              <ListView
+                dataSource={this.state.dataSource}
+                navigator={navigator}
+                renderRow={(dataSource) => <Row data={dataSource} navigator={navigator} />}></ListView>
+            );
+          case 1:
+            return (
+              <View><Text>Cool</Text></View>
+            )
+        }
+      }}></Navigator>
     );
 
   }
