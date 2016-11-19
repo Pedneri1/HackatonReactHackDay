b14a3c7134f0d65170d62a05fdb6d26abda82908
Victor <victor.freitas.3005@gmail.com>
2016-11-19 16:03:35 -0200

    Feed file started

---
+++ adopt/Feed.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+class Feed extends Component {
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    };
+  }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <Text>{rowData}</Text>}
+      />
+    );
+  }
+}
