226a7f12ed4859b4de73867f5e2f7bf303bad4b6
pedroneri1 <pedneri1@gmail.com>
2016-11-19 15:12:12 -0300

    Added organization

--- back-end-adop/database/migrations/2016_11_19_174538_create_animal_table.php
+++ back-end-adop/database/migrations/2016_11_19_174538_create_animal_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAnimalTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('animals', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nome');
-            $table->string('email')->unique();
-            $table->string('senha');
-            $table->string('whatsapp');
+            $table->string('raca');
+            $table->string('especie');
+            $table->string('endereco');
+            $table->string('nascimento');
+            $table->string('sexo');
+            $table->string('descricao');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -31,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
     }
 }
