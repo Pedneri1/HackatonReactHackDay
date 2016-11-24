432c144668b831621a9a25c4ae588dbe7bc2aeaa
Cássio Santos <sss.cassio@gmail.com>
2016-11-24 17:21:56 -0300

    Re-organizando pastas

--- adopt/index.android.js
+++ adopt/index.android.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './components/App';
 
 export default class adopt extends Component {
   render() {
